98a33a0261ba60d1685c82ae98c8502ba8136eac
kabum <uu.kabum@gmail.com>
2010-11-16 17:56:07 +0100

    first model build

---
+++ data/sql/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE games (id INTEGER PRIMARY KEY AUTOINCREMENT, white_id INTEGER NOT NULL, black_id INTEGER NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_forgot_password (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, unique_key VARCHAR(255), expires_at DATETIME NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_group (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) UNIQUE, description VARCHAR(1000), created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_group_permission (group_id INTEGER, permission_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(group_id, permission_id));
+CREATE TABLE sf_guard_permission (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) UNIQUE, description VARCHAR(1000), created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_remember_key (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, remember_key VARCHAR(32), ip_address VARCHAR(50), created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_user (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(255), last_name VARCHAR(255), email_address VARCHAR(255) NOT NULL UNIQUE, username VARCHAR(128) NOT NULL UNIQUE, algorithm VARCHAR(128) DEFAULT 'sha1' NOT NULL, salt VARCHAR(128), password VARCHAR(128), is_active INTEGER DEFAULT '1', is_super_admin INTEGER DEFAULT '0', last_login DATETIME, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_user_group (user_id INTEGER, group_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(user_id, group_id));
+CREATE TABLE sf_guard_user_permission (user_id INTEGER, permission_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(user_id, permission_id));
+CREATE INDEX is_active_idx_idx ON sf_guard_user (is_active);
